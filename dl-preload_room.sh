71177f3a5af320de395193b936baf2f4fecc90e0
April Kahny <aprilkahny@gmail.com>
2021-07-17 22:14:55 -0400

    add script to download preload_room

---
+++ dl-preload_room.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl 'https://uc7e53fe0e65dfefb52de92e3fef.dl.dropboxusercontent.com/cd/0/get/BShmJJ9qR6XXpGw6DY_-SbRziDi4b33X5Fy2vUkvijxH5Px-9aKLS4JpPqej1pVXr32w-UcUaWfmOQJq6jTFheQ0_LKO7UWTlBy69JcN-z4lb4YYJmFQoOM4ofTVDOJU1edDkNOjeWBEhbT__Rf-RxPC/file?_download_id=797825820330605276868957215044008447313519878427108530814276777&_notify_domain=www.dropbox.com&dl=1' > ../maps/preload_room.bsp
